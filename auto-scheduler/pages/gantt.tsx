66ccc89761e2ffbcb6de72b28f6d13c2725bd462
shunkat <shunkat@icloud.com>
2022-04-07 21:32:49 +0900

    プロジェクト初期化

--- auto-scheduler/pages/gantt.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import dynamic from "next/dynamic";
-
-const GanttChart: NextPage = () => {
-  const Gantt = dynamic(() => import("../components/gantt"));
-  return <Gantt />;
-};
-
-export default GanttChart;
