ab9976c7268d067a04cbe464717f86c1d6611b82
yotubarail <shota_t@i.softbank.jp>
2022-04-10 01:35:15 +0900

    表の基本的なパーツの配置

--- pages/tableView/tableVIew.tsx
+++ pages/tableView/tableVIew.tsx
@@ -3,9 +3,31 @@ import tableStyles from '../../styles/table.module.scss'
 
 const TableView: NextPage = () => {
   return (
-    <div>
-      <p className='text-2xl'>テスト</p>
-    </div>
+    <table className="table-auto">
+      <thead className='bg-gray-300'>
+        <tr>
+          <th colSpan={5} className="border border-gray-500">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded">プロジェクト追加＋</button>
+          </th>
+        </tr>
+        <tr>
+          <th className="border border-gray-500"></th>
+          <th className="border border-gray-500">件名</th>
+          <th className="border border-gray-500">開始</th>
+          <th className="border border-gray-500">終了</th>
+          <th className="border border-gray-500">先行タスク</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border border-gray-500"></td>
+          <td className="border border-gray-500"></td>
+          <td className="border border-gray-500"></td>
+          <td className="border border-gray-500"></td>
+          <td className="border border-gray-500"></td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
