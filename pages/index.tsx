bba37066906e722dfa9ad4872029758707e7beb9
yotubarail <shota_t@i.softbank.jp>
2022-04-10 01:11:12 +0900

    Tailwind CSSを使えるか確認

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import TableView from './tableView/tableVIew'
 
 const Home: NextPage = () => {
   return (
@@ -51,6 +52,7 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
+        <TableView/>
       </main>
 
       <footer className={styles.footer}>
---
+++ pages/tableView/tableVIew.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import tableStyles from '../../styles/table.module.scss'
+
+const TableView: NextPage = () => {
+  return (
+    <div>
+      <p className='text-2xl'>テスト</p>
+    </div>
+  )
+}
+
+export default TableView;
