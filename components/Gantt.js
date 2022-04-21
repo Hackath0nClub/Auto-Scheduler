cf831cb568872631def9bbc6cd95a85f6c55bb99
kou72 <genpokopokopoko@gmail.com>
2022-04-21 16:23:14 +0900

    微修正

--- components/Gantt.js
+++ components/Gantt.js
@@ -3,6 +3,8 @@ import { gantt } from "dhtmlx-gantt";
 import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
 import { ScheduleDataContext } from "./ScheduleDataProvider";
 
+// DHTMLXのブログに従って導入
+// https://dhtmlx.com/blog/create-react-gantt-chart-component-dhtmlxgantt/?utm_source=trial_html&utm_medium=referral&utm_campaign=gantt
 class DhtmlxGantt extends Component {
   componentDidMount() {
     gantt.config.date_format = "%Y-%m-%d %H:%i";
@@ -25,12 +27,7 @@ class DhtmlxGantt extends Component {
 
 const Gantt = () => {
   const [scheduleData, setScheduleData] = useContext(ScheduleDataContext);
-
-  return (
-    <>
-      <DhtmlxGantt tasks={scheduleData} />
-    </>
-  );
+  return <DhtmlxGantt tasks={scheduleData} />;
 };
 
 export default Gantt;
