2fdf4321ac3ad34bce02291ce8f60ba4eeffca59
kou72 <genpokopokopoko@gmail.com>
2022-05-12 03:03:34 +0900

    バグ修正, リファクタリング

--- react_app/components/Gantt.tsx
+++ react_app/components/Gantt.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { gantt } from "dhtmlx-gantt";
 import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
 import { ScheduleDataContext } from "./ScheduleDataProvider";
@@ -6,7 +6,51 @@ import { ScheduleDataContext } from "./ScheduleDataProvider";
 const Gantt = () => {
   const [scheduleData, setScheduleData] = useContext(ScheduleDataContext);
 
-  useEffect(() => {
+  const searchDependLinks = (id: number, targetIds: number[]) => {
+    const dependLinks = gantt.getLinks().filter((link) => link.source == id);
+    // 依存するIDを全て取得するまで(リンク先が得られなくなるまで)再帰呼び出しを行う
+    if (dependLinks != []) {
+      dependLinks.map((link) => searchDependLinks(link.target, targetIds));
+    }
+    dependLinks.map((dependLink) => targetIds.push(dependLink.target));
+    return targetIds;
+  };
+
+  const addTaskDate = (task: any, date: number) => {
+    const formatFunc = gantt.date.date_to_str("%Y-%m-%d %H:%i");
+    const addDate = (tgt: any) => gantt.date.add(tgt, date / 60000, "minute");
+    task.start_date = formatFunc(addDate(task.start_date));
+    task.end_date = formatFunc(addDate(task.end_date));
+    return task;
+  };
+
+  const createUpdateData = (linkIds: number[], dragDate: number) => {
+    const ganttData = gantt.serialize();
+    ganttData.data = ganttData.data.map((task: any) => {
+      if (linkIds.includes(task.id)) {
+        task = addTaskDate(task, dragDate);
+      }
+      return task;
+    });
+    return ganttData;
+  };
+
+  let beforStartDate: number = 0;
+
+  const setBeforStartDate = (id: number) => {
+    beforStartDate = gantt.getTask(id).start_date.getTime();
+    return true;
+  };
+
+  const updateScheduleData = (id: number) => {
+    const linkIds = searchDependLinks(id, []);
+    const afterStartDate: number = gantt.getTask(id).start_date.getTime();
+    const dragDate = afterStartDate - beforStartDate;
+    const updateData = createUpdateData(linkIds, dragDate);
+    setScheduleData(updateData);
+  };
+
+  const initializeGantt = () => {
     gantt.config.date_format = "%Y-%m-%d %H:%i";
     gantt.i18n.setLocale("jp");
     gantt.config.columns = [
@@ -18,68 +62,17 @@ const Gantt = () => {
       { name: "add", label: "", width: 30 },
     ];
     gantt.init("gantt");
-  }, []);
+    gantt.attachEvent("onBeforeTaskDrag", (id) => setBeforStartDate(id), {});
+    gantt.attachEvent("onAfterTaskDrag", (id) => updateScheduleData(id), {});
+  };
 
-  useEffect(() => {
+  const renderGantt = () => {
     gantt.parse(scheduleData);
     gantt.render();
-  });
-
-  let targetIds: number[] = [];
-  let beforStartDate: Date;
-  let afterStartDate: Date;
-
-  const searchRecursiveDependLinks = (id: number) => {
-    const links = gantt.getLinks();
-    const dependLinks = links.filter((link) => link.source == id);
-    if (dependLinks != []) {
-      dependLinks.map((link) => searchRecursiveDependLinks(link.target));
-    }
-    dependLinks.map((dependLink) => targetIds.push(dependLink.target));
   };
 
-  const addDate = (task: any, date: number) => {
-    const formatFunc = gantt.date.date_to_str("%Y-%m-%d %H:%i");
-    return formatFunc(gantt.date.add(task.start_date, date / 60000, "minute"));
-  };
-
-  const createUpdateData = () => {
-    const currentData = gantt.serialize();
-    const dragDate = afterStartDate.getTime() - beforStartDate.getTime();
-    const addDateData = currentData.data.map((task: any) => {
-      if (targetIds.includes(task.id)) {
-        task.start_date = addDate(task, dragDate);
-        task.end_date = addDate(task, dragDate);
-      }
-      return task;
-    });
-    const updateData = {
-      data: addDateData,
-      links: currentData.links,
-    };
-    return updateData;
-  };
-
-  gantt.attachEvent(
-    "onBeforeTaskDrag",
-    function (id) {
-      targetIds = [];
-      beforStartDate = gantt.getTask(id).start_date;
-      return true;
-    },
-    {}
-  );
-
-  gantt.attachEvent(
-    "onAfterTaskDrag",
-    function (id) {
-      afterStartDate = gantt.getTask(id).start_date;
-      searchRecursiveDependLinks(id);
-      const updateData = createUpdateData();
-      setScheduleData(updateData);
-    },
-    {}
-  );
+  useEffect(initializeGantt, []);
+  useEffect(renderGantt, [scheduleData]);
 
   return (
     <>
