96ac33c59e87852f7c56dc1f7d8e8edc04df959a
kou72 <genpokopokopoko@gmail.com>
2022-05-15 14:24:01 +0900

    ログイン機能実装

--- react_app/components/Header.tsx
+++ react_app/components/Header.tsx
@@ -19,7 +19,7 @@ export const Header = () => {
           </div>
           <div>
             <a
-              href="#"
+              href="/api/auth/login"
               className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
             >
               Login
