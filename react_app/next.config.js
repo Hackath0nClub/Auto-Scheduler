a5430b07e2b8c38858d066ab85ae053e82607126
kou72 <genpokopokopoko@gmail.com>
2022-05-16 21:32:10 +0900

    next-transpile-modules remove, next.config.jsの戻し

--- react_app/next.config.js
+++ react_app/next.config.js
@@ -2,9 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-const withTM = require("next-transpile-modules")([
-  "frappe-gantt-react",
-  "frappe-gantt",
-]);
 
-module.exports = withTM({});
+module.exports = nextConfig;
