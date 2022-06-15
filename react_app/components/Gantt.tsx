87ab134723283d5337369f83eb535372111cda9c
yotubarail <shota_t@i.softbank.jp>
2022-06-15 22:31:18 +0900

    ガントの日付表示設定

--- react_app/components/Gantt.tsx
+++ react_app/components/Gantt.tsx
@@ -5,6 +5,47 @@ import { ScheduleDataContext } from "./ScheduleDataProvider";
 
 const Gantt = () => {
   const [scheduleData, setScheduleData] = useContext(ScheduleDataContext);
+
+  // ガント初期設定
+  const initializeGantt = () => {
+    gantt.config.date_format = "%Y-%m-%d %H:%i";
+    gantt.config.drag_project = true;
+    gantt.i18n.setLocale("jp");
+    gantt.config.start_date = new Date("2000, 01, 01");
+    gantt.config.end_date = new Date("2100, 12, 31");
+    gantt.config.scales = [
+      {unit: "month", step: 1, format: "%Y年%F"},
+      {unit: "day", step:1, format: "%j日(%D)" }
+  ];
+    gantt.config.columns = [
+      { name: "number", label: "", width: 20 },
+      { name: "text", label: "件名", width: 200, tree: true, align: "center" },
+      { name: "start_date", label: "開始", align: "center", width: 100 },
+      { name: "end_date", label: "終了", align: "center", width: 100 },
+      { name: "", label: "先行タスク", width: 200 },
+      { name: "add", label: "", width: 30 },
+    ];
+    gantt.init("gantt");
+    // タスクドラック操作に処理アタッチ
+    gantt.attachEvent("onBeforeTaskDrag", (id) => setBeforeStartDate(id), {});
+    gantt.attachEvent("onAfterTaskDrag", (id) => updateScheduleData(id), {});
+    // タスク選択フォームからの変更に処理をアタッチ
+    gantt.attachEvent("onLightbox", (id) => setBeforeStartDate(id), {});
+    gantt.attachEvent("onLightboxSave", (id) => updateScheduleDataOnTable(id), {});
+    // リンク操作に処理アタッチ
+    gantt.attachEvent("onAfterLinkAdd", () => updateScheduleLinks(), {});
+    gantt.attachEvent("onAfterLinkDelete", () => updateScheduleLinks(), {});
+  };
+
+  const renderGantt = () => {
+    gantt.parse(scheduleData);
+    gantt.render();
+  };
+
+  useEffect(initializeGantt, []);
+  useEffect(renderGantt, [scheduleData]);
+
+
   const searchDependLinks = (id: number, targetIds: number[]) => {
     // getLinks()で取得するsource, targetがString型なので、numberに変換
     const dependLinks = gantt.getLinks().map((dependLink) => {
@@ -92,40 +133,6 @@ const Gantt = () => {
     setScheduleData(updateData);
     return true;
   };
-
-  const initializeGantt = () => {
-    gantt.config.date_format = "%Y-%m-%d %H:%i";
-    gantt.config.drag_project = true;
-    gantt.i18n.setLocale("jp");
-    // gantt.config.start_date = new Date("2018, 08, 10");
-    // gantt.config.end_date = new Date("2022, 08, 20");
-    gantt.config.columns = [
-      { name: "number", label: "", width: 20 },
-      { name: "text", label: "件名", width: 200, tree: true, align: "center" },
-      { name: "start_date", label: "開始", align: "center", width: 100 },
-      { name: "end_date", label: "終了", align: "center", width: 100 },
-      { name: "", label: "先行タスク", width: 200 },
-      { name: "add", label: "", width: 30 },
-    ];
-    gantt.init("gantt");
-    // タスクドラック操作に処理アタッチ
-    gantt.attachEvent("onBeforeTaskDrag", (id) => setBeforeStartDate(id), {});
-    gantt.attachEvent("onAfterTaskDrag", (id) => updateScheduleData(id), {});
-    // タスク選択フォームからの変更に処理をアタッチ
-    gantt.attachEvent("onLightbox", (id) => setBeforeStartDate(id), {});
-    gantt.attachEvent("onLightboxSave", (id) => updateScheduleDataOnTable(id), {});
-    // リンク操作に処理アタッチ
-    gantt.attachEvent("onAfterLinkAdd", () => updateScheduleLinks(), {});
-    gantt.attachEvent("onAfterLinkDelete", () => updateScheduleLinks(), {});
-  };
-
-  const renderGantt = () => {
-    gantt.parse(scheduleData);
-    gantt.render();
-  };
-
-  useEffect(initializeGantt, []);
-  useEffect(renderGantt, [scheduleData]);
 
   return (
     <>
